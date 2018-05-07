7eba441726363b745e4042084c7a3c394bba0eb6
kimeasyn <kimeasyn@gmail.com>
2018-05-07 20:19:24 +0900

    FIX: 주석 수정

--- scraper/scripts/music_scraper.py
+++ scraper/scripts/music_scraper.py
@@ -6,12 +6,13 @@ from scraper.models import WebMusic
 
 def run():
     """
-    네이버 웹툰에서 인기 급상승 웹툰 10개 제목만 긁어서 저장
-    실행 명령어: manage.py 가 있는 dir에서 [python manage.py runscript -v2 webtoon_scraper]
+    멜론(melon.com)에서 일간 인기 차트에서 노래 10개의 순위, 가수, 제목 크롤링해와서 저장
+    실행 명령어: manage.py 가 있는 dir에서 [python manage.py runscript -v2 music_scraper]
     """
     melon = requests.get('https://www.melon.com/chart/index.htm')  # 멜론차트 웹사이트
     melon_html = melon.text                                        # 멜론 차트의 html 구성
-    melon_parse = BeautifulSoup(melon_html, 'html.parser')         # 멜론 차트의 html 구성을 html 파싱
+    melon_parse = BeautifulSoup(melon_html, 'html.parser')
+    # 멜론 차트의 html 구성을 html selector로 접근할 수 있도록 html 파싱
     title_html = melon_parse.select('#lst50 > td > div > div > div.ellipsis.rank01 > span > a')
     # 멜론 차트의 노래 제목 html selector 선택
     singer_html = melon_parse.select('#lst50 > td > div > div > div.ellipsis.rank02 > span')
--- scraper/scripts/news_scraper.py
+++ scraper/scripts/news_scraper.py
@@ -4,6 +4,10 @@ from scraper.models import WebNews
 
 
 def run():
+    """
+    네이버 뉴스에서 오늘의 뉴스 10개 제목만 긁어서 저장
+    실행 명령어: manage.py 가 있는 dir에서 [python manage.py runscript -v2 news_scraper]
+    """
     url = 'http://news.naver.com/'
     # 크롤링할 주소
     res = rq.urlopen(url)
@@ -11,7 +15,7 @@ def run():
     soup = BeautifulSoup(res, "html.parser")
     # res에서 html요소들을 불러와서 파싱
     news_title_html = soup.select('li > div > a > strong')
-    # 불러온 html요소들 중 li > div > a >  strong 에 해당하는 요소만 선택
+    # 불러온 html요소들 중 li > div > a >  strong(네이버뉴스 - 오늘의 뉴스제목의 html selector) 에 해당하는 요소만 선택
 
     for html in news_title_html:  # 여러개의 뉴스 제목들을 for문으로 각각 선택
         news_title = html.get_text()    # html 태그를 제외한 text만 선택
